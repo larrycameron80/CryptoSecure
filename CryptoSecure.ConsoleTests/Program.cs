2bf1ef29aa9545bc434c6ef27bbcf15d79915c62
mulanecka <mulanecka@gmail.com>
2017-06-29 14:01:09 +0200

    Newly created CryptoSecure library added.

---
+++ CryptoSecure.ConsoleTests/Program.cs
@@ -0,0 +1,91 @@
+﻿using CryptoSecure.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoSecure.ConsoleTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+
+            Console.WriteLine();
+            Console.WriteLine("Please enter an option number : ");
+            string option = Console.ReadLine();
+            Console.WriteLine();
+
+            while (option != "2")
+            {
+                switch (option)
+                {
+                    case "1":
+                        EncryptDecryptPassword();
+                        break;
+                    case "2":
+                        Console.ReadKey();
+                        break;
+                    default:
+                        break;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Please enter an option number : ");
+                option = Console.ReadLine();
+                Console.WriteLine();
+            }
+        }
+
+        private static void Menu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("---- MENU ----");
+            Console.WriteLine("Select an option from menu:");
+            Console.WriteLine("1: Encrypt / decrypt string");
+            Console.WriteLine("2: Exit");
+        }
+
+        private static void EncryptDecryptPassword()
+        {
+            Console.WriteLine("---- Encrypt password ----");
+            Console.WriteLine();
+
+            Console.WriteLine("Please enter a password to use:");
+            string password = Console.ReadLine();
+
+            string salt = PasswordAuthentication.GenerateSalt();
+
+            string hash = PasswordAuthentication.GenerateHash(salt, password);
+
+            Console.WriteLine("Your encrypted password : {0}", hash);
+
+            Console.WriteLine();
+            Console.WriteLine("---- Decrypt password ----");
+            Console.WriteLine();
+
+            Console.WriteLine("Please enter your password:");
+            string newPassword = Console.ReadLine();
+
+            string newHash = PasswordAuthentication.GenerateHash(salt, newPassword);
+
+            Console.WriteLine(string.Format("New password hash: {0}", newHash));
+
+            bool passwordsMatch = PasswordAuthentication.CompareHashes(hash, newHash);
+
+            if (passwordsMatch)
+            {
+                Console.WriteLine("Given password matches the original.");
+            }
+            else
+            {
+                Console.WriteLine("Given password does not match the original.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CryptoSecure/Concrete/PasswordAuthentication.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoSecure.Concrete
+{
+    public static class PasswordAuthentication
+    {
+        private const int DerivedKeyLength = 24;
+
+        public static string GenerateSalt()
+        {
+            try
+            {
+                using (RandomNumberGenerator rng = new RNGCryptoServiceProvider())
+                {
+                    byte[] tokenData = new byte[32];
+                    rng.GetBytes(tokenData);
+
+                    return Convert.ToBase64String(tokenData);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(string.Format("An error occured while generating salt. Exception : {0}", ex.Message), ex);
+            }
+        }
+
+        public static string GenerateHash(string salt, string password)
+        {
+            try
+            {
+                byte[] hashValue;
+
+                var valueToHash = string.IsNullOrEmpty(password) ? string.Empty : password;
+
+                var saltInBytes = Encoding.ASCII.GetBytes(salt);
+
+                using (var pbkdf2 = new Rfc2898DeriveBytes(valueToHash, saltInBytes))
+                {
+                    hashValue = pbkdf2.GetBytes(DerivedKeyLength);
+                }
+
+                return Convert.ToBase64String(hashValue);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(string.Format("An error occured while generating hash. Exception : {0}", ex.Message), ex);
+            }
+        }
+
+        public static bool CompareHashes(string originalHash, string newHash)
+        {
+            if (string.Equals(originalHash, newHash))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
